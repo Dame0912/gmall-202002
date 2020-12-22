a06cbaafb9866ad7b44dccb75c6d2ff3352c737b
liyuqing <liyuqing09121029>
2020-12-22 16:09:31 +0800

    初始化

---
+++ gulimall-ware/src/test/java/com/dame/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dame.gulimall.ware;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GulimallWareApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
